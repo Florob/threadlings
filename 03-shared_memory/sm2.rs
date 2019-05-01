5402150792628cea484abf47023bf9e69444c4db
Florian Zeitz <florob@babelmonkeys.de>
2019-05-01 18:39:51 +0200

    Add shared memory exercise for atomic types

---
+++ 03-shared_memory/sm2.rs
@@ -0,0 +1,52 @@
+// sm2.rs
+// Make this compile, without using a Mutex
+// Scroll down for hints
+
+use std::sync::Arc;
+use std::thread;
+
+const NUM_THREADS: u32 = 4;
+const INCREMENTS: u32 = 1_000_000;
+
+fn main() {
+    let counter = Arc::new(0u32);
+
+    let handles: Vec<_> = (0..NUM_THREADS).map(|_| {
+        let thread_counter = counter.clone();
+        thread::spawn(move || {
+            for _ in 0..INCREMENTS {
+                *thread_counter += 1;
+            }
+        })
+    }).collect();
+
+    for h in handles { h.join().unwrap(); }
+
+    assert_eq!(NUM_THREADS * INCREMENTS, *counter);
+    println!("Success!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The types in the `std::sync::atomic` module provide shared mutable access for some types.
